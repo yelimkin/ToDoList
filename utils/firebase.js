e553e89148ab6c884aba7c2caacffbae42a36a06
yelimkin <yelim785@gmail.com>
2024-07-24 14:00:37 +0900

    firebaseConfig 수정

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA02uS14cQNSoglongbuGGAux-Zq5cVm6s',
-  authDomain: 'to-do-list-app-e9e26.firebaseapp.com',
-  projectId: 'to-do-list-app-e9e26',
-  storageBucket: 'to-do-list-app-e9e26.appspot.com',
-  messagingSenderId: '800050319851',
-  appId: '1:800050319851:web:6e17bd0f85308b3b9689ae',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
