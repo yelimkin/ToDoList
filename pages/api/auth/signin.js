8a4a6c7d398e548b4ecd4fc052556a1c5c1463c3
yelimkin <yelim785@gmail.com>
2024-08-09 14:53:37 +0900

    데이터베이스 바꾸기 위한 파일 정리

--- pages/api/auth/signin.js
+++
@@ -1,9 +0,0 @@
-import { signIn } from 'next-auth/react';
-
-export default function SignIn() {
-  return (
-    <div>
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-    </div>
-  );
-}
