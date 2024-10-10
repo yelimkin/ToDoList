a49cf4dff8cd442dd0e87b3d29ba67252c6a183d
yelimkin <yelim785@gmail.com>
2024-10-10 22:33:34 +0900

    hotjar 도입 시도 - 4

--- pages/_app.js
+++ pages/_app.js
@@ -11,17 +11,30 @@ import Layout from '../components/Layout';
 // const isProduction = process.env.NODE_ENV === 'production';
 
 function App({ Component, session, ...pageProps }) { // 모든 페이지의 공통 레이아웃을 정의 ({ 현재 렌더링할 페이지 컴포넌트, 해당 페이지 컴포넌트에 전달할 초기 props }) 
-  useEffect(() => {
-    hotjar.initialize(process.env.NEXT_PUBLIC_HOTJAR_ID, process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION);
-  }, []);
-  return (    
-    <SessionProvider session={session}> {/* 세션 상태를 제공하는 컴포넌트로 모든 페이지에서 로그인 상태와 세션 정보를 쉽게 사용, 서버 측에서 세션 정보를 받아와 클라이언트에 전달하는 seesion 속성 사용 */}
-      <TodoProvider> {/* TodoList와 관련된 상태를 제공하는 컨텍스트 공급자로 모든 페이지에서 할 일 목록과 관련된 상태를 쉽게 관리 */}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </TodoProvider>
-    </SessionProvider>
+  return (  
+    <>
+      <head>
+        <script>
+          // @ts-ignore
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5162808,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+      </head>
+      <SessionProvider session={session}> {/* 세션 상태를 제공하는 컴포넌트로 모든 페이지에서 로그인 상태와 세션 정보를 쉽게 사용, 서버 측에서 세션 정보를 받아와 클라이언트에 전달하는 seesion 속성 사용 */}
+        <TodoProvider> {/* TodoList와 관련된 상태를 제공하는 컨텍스트 공급자로 모든 페이지에서 할 일 목록과 관련된 상태를 쉽게 관리 */}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </TodoProvider>
+      </SessionProvider>
+    </>  
+    
   );
   // -> AuthProvider에서 인증 상태 전역 관리
   // -> TodoProvider에서 TodoList 상태 전역 관리
